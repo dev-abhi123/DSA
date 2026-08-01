8310e85db0a4260db32f447cc0be42ecd3b9a419
dev-abhi123 <abhimanyubasu2007@gmail.com>
2026-08-02 00:38:38 +0530

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=5;j++){
-            cout<<j<<" ";
+    int a=1;
+    for(int i=0;i<3;i++){
+       
+        for(int j=0;j<3;j++){
+            cout<<a<<" ";
+            a++;
         }
         cout<< endl;
     }
